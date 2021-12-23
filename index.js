a502e93965de1093421c1c3d966d61767abf76a4
Kiran Pradeep <shindekiran857@gmail.com>
2021-12-23 11:30:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//alert("hello world");
+function openNav(){
+    document.getElementById("mySidebar").style.width="180px";
+    document.getElementById("main").style.marginLeft="180px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
